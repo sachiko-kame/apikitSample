d20edda1c92900f4a2d526b2374ff156e5eac954
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-05 12:23:06 +0900

    ATSの処理の確認としてwebViewを表示してみる。

--- APIKitSample/ViewController.swift
+++ APIKitSample/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let webView = UIWebView()
+        webView.frame = self.view.frame
+        self.view.addSubview(webView)
+        
+        let sampleURL = NSURL(string: "https://www.google.co.jp/")
+        let urlRequest = NSURLRequest(url: sampleURL as! URL)
+        webView.loadRequest(urlRequest as URLRequest)
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
