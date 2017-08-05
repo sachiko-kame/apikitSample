93cd209c551b16ebfb65948410958059879c8f6c
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-05 12:27:17 +0900

    webViweの表示をとりあえずコメントアウトしておく。

--- APIKitSample/ViewController.swift
+++ APIKitSample/ViewController.swift
@@ -13,13 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let webView = UIWebView()
-        webView.frame = self.view.frame
-        self.view.addSubview(webView)
-        
-        let sampleURL = NSURL(string: "https://www.google.co.jp/")
-        let urlRequest = NSURLRequest(url: sampleURL as! URL)
-        webView.loadRequest(urlRequest as URLRequest)
+//        self.sampleWebView()
         
         
         
@@ -31,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+//    func sampleWebView (){
+//        let webView = UIWebView()
+//        webView.frame = self.view.frame
+//        self.view.addSubview(webView)
+//        
+//        let sampleURL = NSURL(string: "https://www.google.co.jp/")
+//        let urlRequest = NSURLRequest(url: sampleURL as! URL)
+//        webView.loadRequest(urlRequest as URLRequest)
+//
+//    }
 
 }
 
