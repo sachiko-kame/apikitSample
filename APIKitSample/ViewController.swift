9f6c56d209222819321c4b5b53001cb32a37d8c6
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-07 23:43:31 +0900

    お手本通りにかく

--- APIKitSample/ViewController.swift
+++ APIKitSample/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 import APIKit
 
+struct RateLimitRequest: Request {
+    typealias Response = RateLimit
+    
+    var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var path: String {
+        return "/rate_limit"
+    }
+    
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> RateLimit {
+        return try RateLimit(object: object)
+    }
+}
+
+//レスポンスの型の定義、保持、ここから値取り出してのちにprintする
+struct RateLimit {
+    let limit: Int
+    let remaining: Int
+    
+    init(object: Any) throws {
+        guard let dictionary = object as? [String: Any],
+            let rateDictionary = dictionary["rate"] as? [String: Any],
+            let limit = rateDictionary["limit"] as? Int,
+            let remaining = rateDictionary["remaining"] as? Int else {
+                throw ResponseError.unexpectedObject(object)
+        }
+        
+        self.limit = limit
+        self.remaining = remaining
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let request = RateLimitRequest()
+        
+        Session.send(request) { result in
+            switch result {
+            case .success(let rateLimit):
+                // Type of `rateLimit` is inferred as `RateLimit`,
+                // which is also known as `RateLimitRequest.Response`.
+                print("limit: \(rateLimit.limit)")
+                print("remaining: \(rateLimit.remaining)")
+                
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+        
 //        self.sampleWebView()
+        
         
         
         
